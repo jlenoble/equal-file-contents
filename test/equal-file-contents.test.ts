4deed656ecf30abec1c083d7cc6199faa35884f8
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-16 10:34:13 +0200

    Converted to async syntax

--- test/equal-file-contents.test.ts
+++ test/equal-file-contents.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-invalid-this */
+
 import gulp from "gulp";
 import babel from "gulp-babel";
 import path from "path";
@@ -8,134 +10,167 @@ import { tmpDir } from "cleanup-wrapper";
 import mkdirp from "mkdirp";
 import touch from "touch";
 
-describe("Testing equalFileContents", () => {
+describe("Testing equalFileContents", (): void => {
   const cwd = process.cwd();
 
-  describe(`Pipe is noop()`, () => {
-    it(`equalFileContents returns a promise that resolves on equality`, () => {
+  describe(`Pipe is noop()`, (): void => {
+    it(`equalFileContents returns a promise that resolves on equality`, (): Promise<
+      boolean
+    > => {
       return equalFileContents("gulp/**/*.js", ".");
     });
 
-    it(`equalFileContents returns a promise that rejects on inequality`, () => {
-      return equalFileContents("gulp/**/*.js", "src").catch(err => {
+    it(`equalFileContents returns a promise that rejects on inequality`, async (): Promise<
+      void
+    > => {
+      try {
+        await equalFileContents("gulp/**/*.js", "src");
+      } catch (err) {
         expect(err.message).to.match(/expected .* to equal/);
-      });
+      }
     });
   });
 
-  describe(`Pipe is babel()`, () => {
+  describe(`Pipe is babel()`, (): void => {
     it(
       `equalFileContents returns a promise that resolves on equality`,
-      tmpDir("tmp", () => {
-        return streamToPromise(
-          gulp
-            .src("gulp/**/*.js", { base: cwd })
-            .pipe(babel())
-            .pipe(gulp.dest("tmp"))
-        ).then(() => equalFileContents("gulp/**/*.js", "tmp", babel));
-      })
+      tmpDir(
+        "tmp",
+        async (): Promise<boolean> => {
+          await streamToPromise(
+            gulp
+              .src("gulp/**/*.js", { base: cwd })
+              .pipe(babel())
+              .pipe(gulp.dest("tmp"))
+          );
+
+          return equalFileContents("gulp/**/*.js", "tmp", babel);
+        }
+      )
     );
 
     it(
       `equalFileContents returns a promise that rejects on inequality`,
-      tmpDir("tmp", () => {
-        return streamToPromise(
-          gulp.src("gulp/**/*.js", { base: cwd }).pipe(gulp.dest("tmp"))
-        )
-          .then(() => equalFileContents("gulp/**/*.js", "tmp", babel))
-          .catch(err => {
+      tmpDir(
+        "tmp",
+        async (): Promise<void> => {
+          await streamToPromise(
+            gulp.src("gulp/**/*.js", { base: cwd }).pipe(gulp.dest("tmp"))
+          );
+
+          try {
+            await equalFileContents("gulp/**/*.js", "tmp", babel);
+          } catch (err) {
             expect(err.message).to.match(/expected .* to equal/);
-          });
-      })
+          }
+        }
+      )
     );
   });
 
-  describe(`Dest is outside the package directory tree`, () => {
-    beforeEach(function() {
+  describe(`Dest is outside the package directory tree`, (): void => {
+    beforeEach(function(): void {
       this.dest = "/tmp/equalFileContents-test_" + new Date().getTime();
     });
 
-    it(`equalFileContents returns a promise that resolves on equality`, function() {
-      return streamToPromise(
+    it(`equalFileContents returns a promise that resolves on equality`, async function(): Promise<
+      boolean
+    > {
+      await streamToPromise(
         gulp.src("gulp/**/*.js", { base: cwd }).pipe(gulp.dest(this.dest))
-      ).then(() => equalFileContents("gulp/**/*.js", this.dest));
+      );
+
+      return equalFileContents("gulp/**/*.js", this.dest);
     });
 
-    it(`equalFileContents returns a promise that rejects on inequality`, function() {
-      return streamToPromise(
+    it(`equalFileContents returns a promise that rejects on inequality`, async function(): Promise<
+      void
+    > {
+      await streamToPromise(
         gulp.src("gulp/**/*.js", { base: cwd }).pipe(gulp.dest(this.dest))
-      )
-        .then(() => equalFileContents("gulp/**/*.js", this.dest, babel))
-        .catch(err => {
-          expect(err.message).to.match(/expected .* to equal/);
-        });
+      );
+
+      try {
+        await equalFileContents("gulp/**/*.js", this.dest, babel);
+      } catch (err) {
+        expect(err.message).to.match(/expected .* to equal/);
+      }
     });
   });
 
-  describe(`Dest is deeper inside the package directory tree`, () => {
+  describe(`Dest is deeper inside the package directory tree`, (): void => {
     const base = "build_" + new Date().getTime();
 
-    beforeEach(function() {
+    beforeEach(function(): void {
       this.src = path.join(base, "src");
       this.dest = path.join(base, "build");
     });
 
     it(
       `equalFileContents returns a promise that resolves on equality`,
-      tmpDir(base, function() {
-        return streamToPromise(
+      tmpDir(base, async function(): Promise<void> {
+        // @ts-ignore
+        const { src, dest }: { src: string; dest: string } = this;
+
+        await streamToPromise(
           gulp.src("src/**/*.js", { base: cwd }).pipe(gulp.dest(base))
-        )
-          .then(() =>
-            streamToPromise(
-              gulp
-                .src(path.join(this.src, "**/*.js"), { base })
-                .pipe(gulp.dest(this.dest))
-            )
-          )
-          .then(() =>
-            equalFileContents(
-              path.join(this.src, "**/*.js"),
-              this.dest,
-              undefined,
-              base
-            )
-          );
+        );
+
+        await streamToPromise(
+          gulp.src(path.join(src, "**/*.js"), { base }).pipe(gulp.dest(dest))
+        );
+
+        await equalFileContents(
+          path.join(src, "**/*.js"),
+          dest,
+          undefined,
+          base
+        );
       })
     );
   });
 
-  describe("Testing with more than 16 files", () => {
+  describe("Testing with more than 16 files", (): void => {
     it(
       `equalFileContents returns a promise that resolves on equality`,
-      tmpDir("tmp", () => {
-        mkdirp.sync("tmp");
-        for (let i = 0; i < 20; i++) {
-          touch.sync("tmp/a" + i);
+      tmpDir(
+        "tmp",
+        (): Promise<boolean> => {
+          mkdirp.sync("tmp");
+          for (let i = 0; i < 20; i++) {
+            touch.sync("tmp/a" + i);
+          }
+          return equalFileContents("tmp/*", ".");
         }
-        return equalFileContents("tmp/*", ".");
-      })
+      )
     );
 
     it(
       `equalFileContents returns a promise that rejects on inequality`,
-      tmpDir(["tmp", "tmp1"], () => {
-        mkdirp.sync("tmp");
-        mkdirp.sync("tmp1/tmp");
-        for (let i = 0; i < 20; i++) {
-          touch.sync("tmp/a" + i);
-          touch.sync("tmp1/tmp/a" + i);
-        }
-        return equalFileContents(["tmp/*", "!tmp/a0"], "tmp1").catch(err => {
-          try {
-            expect(err.toString()).to.match(
-              /AssertionError: expected \{ Object.* to deeply equal \{ Object/
-            );
-          } catch (e) {
-            throw err;
+      tmpDir(
+        ["tmp", "tmp1"],
+        async (): Promise<void> => {
+          mkdirp.sync("tmp");
+          mkdirp.sync("tmp1/tmp");
+
+          for (let i = 0; i < 20; i++) {
+            touch.sync("tmp/a" + i);
+            touch.sync("tmp1/tmp/a" + i);
           }
-        });
-      })
+
+          try {
+            await equalFileContents(["tmp/*", "!tmp/a0"], "tmp1");
+          } catch (err) {
+            try {
+              expect(err.toString()).to.match(
+                /AssertionError: expected \{ Object.* to deeply equal \{ Object/
+              );
+            } catch (e) {
+              throw err;
+            }
+          }
+        }
+      )
     );
   });
 });
